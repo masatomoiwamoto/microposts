ecf0bcc06d125e0b966cd98166d25009c16e556a
MasatomoIwamoto <ragnus-sphere@hotmail.co.jp>
2016-09-15 16:21:37 +0000

    add posting microposts

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:edit, :update, :show ,:create ]
+  resources :microposts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
