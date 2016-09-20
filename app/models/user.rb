edbae553e0bce65d4da0f891088ee23a57f23a43
MasatomoIwamoto <ragnus-sphere@hotmail.co.jp>
2016-09-20 16:33:51 +0000

    first commit rails-i18n add user fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   before_save { self.email = self.email.downcase }
       validates :name, presence: true, length: { maximum: 50 }
+      validates :password, presence: true, length: { maximum: 20 }
+      validates :password_confirmation, presence: true, length: { maximum: 20 }
       validates :profile, presence: true, length: { maximum: 300 }, on: :update
       validates :country, presence: true, length: { maximum: 45 }, on: :update
       validates :region, presence: true, length: { maximum: 30 }, on: :update
